b21f94e13ab5dfdd41065c892f589e9431ad011f
Matthews Chiza Nyirongo <mathews@thezig.io>
2020-11-09 13:40:37 +0200

    Altered main navigation based on signed in user.

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,5 +1,5 @@
 import { AppBar, Container, Link, List, ListItem, ListItemText, makeStyles, Toolbar, Typography } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
     title: {
@@ -24,49 +24,94 @@ const useStyles = makeStyles((theme) => ({
 
 const MainNav = () => {
     const classes = useStyles();
+    const [token, setToken] = useState(null);
 
+    useEffect(() => {
+        setToken(localStorage.getItem('CMSToken'))
+    });
+    
     return (
         <AppBar position="static">
             <Typography variant="h4" className={classes.title}>Online Crime Reporting System</Typography>
             <Toolbar>
                 <Container>
-                    <List component="nav" aria-labelledby="main navigation" className={classes.navDisplayFlex}>
-                        <Link href="/" className={classes.linkText}>
-                            <ListItem button>
-                                <ListItemText primary="HOME" />
-                            </ListItem>
-                        </Link>
-                        <Link href="/about-project" className={classes.linkText}>
-                            <ListItem button>
-                                <ListItemText primary="ABOUT PROJECT" />
-                            </ListItem>
-                        </Link>
-                        <Link href="/all-criminals" className={classes.linkText}>
-                            <ListItem button>
-                                <ListItemText primary="ALL CRIMINALS" />
-                            </ListItem>
-                        </Link>
-                        <Link href="/log-complaint" className={classes.linkText}>
-                            <ListItem button>
-                                <ListItemText primary="LOG COMPLAINT" />
-                            </ListItem>
-                        </Link>
-                        <Link href="/register" className={classes.linkText}>
-                            <ListItem button>
-                                <ListItemText primary="REGISTER" />
-                            </ListItem>
-                        </Link>
-                        <Link href="/login" className={classes.linkText}>
-                            <ListItem button>
-                                <ListItemText primary="LOGIN" />
-                            </ListItem>
-                        </Link>
-                        <Link href="/feedback" className={classes.linkText}>
-                            <ListItem button>
-                                <ListItemText primary="SUBMIT FEEDBACK" />
-                            </ListItem>
-                        </Link>
-                    </List>
+                    {token ? (
+                        <List component="nav" aria-labelledby="main navigation" className={classes.navDisplayFlex}>
+                            <Link href="/" className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary="HOME" />
+                                </ListItem>
+                            </Link>
+                            <Link href="/about-project" className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary="ABOUT PROJECT" />
+                                </ListItem>
+                            </Link>
+                            <Link href="/users/dashboard" className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary="DASHBOARD" />
+                                </ListItem>
+                            </Link>
+                            <Link href="/log-complaint" className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary="LOG COMPLAINT" />
+                                </ListItem>
+                            </Link>
+                            <Link href="/feedback" className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary="SUBMIT FEEDBACK" />
+                                </ListItem>
+                            </Link>
+                            <Link href="/change-password" className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary="CHANGE PASSWORD" />
+                                </ListItem>
+                            </Link>
+                            <Link href="/logout" className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary="LOGOUT" />
+                                </ListItem>
+                            </Link>
+                        </List>
+                    ) : (
+                        <List component="nav" aria-labelledby="main navigation" className={classes.navDisplayFlex}>
+                            <Link href="/" className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary="HOME" />
+                                </ListItem>
+                            </Link>
+                            <Link href="/about-project" className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary="ABOUT PROJECT" />
+                                </ListItem>
+                            </Link>
+                            <Link href="/all-criminals" className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary="ALL CRIMINALS" />
+                                </ListItem>
+                            </Link>
+                            <Link href="/log-complaint" className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary="LOG COMPLAINT" />
+                                </ListItem>
+                            </Link>
+                            <Link href="/register" className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary="REGISTER" />
+                                </ListItem>
+                            </Link>
+                            <Link href="/login" className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary="LOGIN" />
+                                </ListItem>
+                            </Link>
+                            <Link href="/feedback" className={classes.linkText}>
+                                <ListItem button>
+                                    <ListItemText primary="SUBMIT FEEDBACK" />
+                                </ListItem>
+                            </Link>
+                        </List>
+                    )}
                 </Container>
             </Toolbar>
         </AppBar>
