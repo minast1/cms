254c97d3dd3a76001996790581ef37707ca99e01
Matthews Chiza Nyirongo <mathews@thezig.io>
2020-11-26 10:20:38 +0200

    Updated police api to handle adding a police station

--- functions/apis/policeStations.js
+++ functions/apis/policeStations.js
@@ -1,5 +1,48 @@
 const { db } = require('../utils/admin');
 
+exports.addPoliceStation = (request, response) => {
+    if(request.body.stationName.trim() === '') {
+        return response.status(400).json({ message: 'Station name must not be empty' });
+    }
+
+    if(request.body.feedbackNumber.trim() === '') {
+        return response.status(400).json({ message: 'Feedback number must not be empty' });
+    }
+
+    if(request.body.email.trim() === '') {
+        return response.status(400).json({ message: 'Email must not be empty' });
+    }
+
+    if(request.body.addressLine1.trim() === '') {
+        return response.status(400).json({ message: 'Address line 1 Must not be empty' });
+    }
+
+    if(request.body.city.trim() === '') {
+        return response.status(400).json({ message: 'City Id Must not be empty' });
+    }
+
+    const newPoliceStation = {
+        stationName: request.body.stationName,
+        feedbackNumber: request.body.feedbackNumber,
+        email: request.body.email,
+        addressLine1: request.body.addressLine1,
+        addressLine2: request.body.addressLine2,
+        city: request.body.city
+    };
+
+    db
+    .collection('police-stations')
+    .add(newPoliceStation)
+    .then(res => {
+        const stationResponse = newPoliceStation;
+        stationResponse.id = res.id;
+        return response.json(stationResponse);
+    })
+    .catch(error => {
+        return response.status(500).json({ message: error });
+    });
+}
+
 exports.getAllPoliceStations = (request, response) => {
     db
     .collection('police-stations')
@@ -9,11 +52,11 @@ exports.getAllPoliceStations = (request, response) => {
         data.forEach(item => {
             policeStations.push({
                 id: item.id,
-                name: item.data().name,
-                area: item.data().area,
+                stationName: item.data().stationName,
+                addressLine1: item.data().addressLine1,
                 city: item.data().city,
                 email: item.data().email,
-                phoneNumber: item.data().phoneNumber
+                feedbackNumber: item.data().feedbackNumber
             });
         });
         return response.json(policeStations);
